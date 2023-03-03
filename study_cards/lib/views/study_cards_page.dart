418c7f13e3f6cea63177ebd77198fa0f4d1d76db
tobiaszorzetto <66502140+tobiaszorzetto@users.noreply.github.com>
2023-03-03 11:58:22 -0300

    fix: centering cards in study page

--- study_cards/lib/views/study_cards_page.dart
+++ study_cards/lib/views/study_cards_page.dart
@@ -14,14 +14,14 @@ class StudyCardsPage extends StatefulWidget {
   StudyCardsPage({super.key, required this.folder, required this.cardsToStudy});
 
   @override
-  State<StudyCardsPage> createState() => _StudyCardsPageState(folder,cardsToStudy);
+  State<StudyCardsPage> createState() =>
+      _StudyCardsPageState(folder, cardsToStudy);
 }
 
-class _StudyCardsPageState extends State<StudyCardsPage>{
-
+class _StudyCardsPageState extends State<StudyCardsPage> {
   late StudyCardsController controller;
 
-  _StudyCardsPageState(folder,cardsToStudy){
+  _StudyCardsPageState(folder, cardsToStudy) {
     controller = StudyCardsController(folder, cardsToStudy);
   }
 
@@ -34,46 +34,44 @@ class _StudyCardsPageState extends State<StudyCardsPage>{
           onPressed: () => setState(() {
             Navigator.of(context).pop;
             Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => FolderPage(folder: controller.folder)
-            ));
+                builder: (context) => FolderPage(folder: controller.folder)));
           }),
         ),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          IconButton(
-            onPressed: () => setState(() {
-              controller.lastCard();
-            }), 
-            icon: const Icon(Icons.arrow_back_ios_new)
-          ),
-          controller.cardsToStudy.length != controller.cardsStudied.length? _showCard() : const Text("No more cards to study"),
-          IconButton(
-            onPressed: () => setState(() {
-              controller.nextCard();
-            }),  
-            icon: const Icon(Icons.arrow_forward_ios)
-          ),
-        ],
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () => setState(() {
+                      controller.lastCard();
+                    }),
+                icon: const Icon(Icons.arrow_back_ios_new)),
+            controller.cardsToStudy.length != controller.cardsStudied.length
+                ? _showCard()
+                : const Text("No more cards to study"),
+            IconButton(
+                onPressed: () => setState(() {
+                      controller.nextCard();
+                    }),
+                icon: const Icon(Icons.arrow_forward_ios)),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _showCard(){
-
+  Widget _showCard() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
-          height: MediaQuery.of(context).size.height/1.5,
-          width: MediaQuery.of(context).size.width/2,
+          height: MediaQuery.of(context).size.height / 1.5,
+          width: MediaQuery.of(context).size.width / 2,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              controller.showAnswer? _showBack():_showFront()
-            ],
+            children: [controller.showAnswer ? _showBack() : _showFront()],
           ),
         ),
       ],
@@ -82,53 +80,64 @@ class _StudyCardsPageState extends State<StudyCardsPage>{
 
   Visibility _showBack() {
     return Visibility(
-                visible: controller.showAnswer,
-                child: Expanded(
-                  child: Card(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () => setState(() {
-                              controller.showAnswer = !controller.showAnswer;
-                            }), 
-                            child: const Icon(Icons.arrow_upward)
-                          ),
-                        ),
-                        Expanded(flex: 10,child: _showImageInDialog(controller.backCardFile, controller.cardsToStudy[controller.indexCardShowing],controller.cardsToStudy[controller.indexCardShowing].backDescription)),
-                        Expanded(flex: 2,child: _showDificultyOptions())
-                      ],
-                    ),
-                  ),
-                ),
-              );
+      visible: controller.showAnswer,
+      child: Expanded(
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () => setState(() {
+                          controller.showAnswer = !controller.showAnswer;
+                        }),
+                    child: const Icon(Icons.arrow_upward)),
+              ),
+              Expanded(
+                  flex: 10,
+                  child: _showImageInDialog(
+                      controller.backCardFile,
+                      controller.cardsToStudy[controller.indexCardShowing],
+                      controller.cardsToStudy[controller.indexCardShowing]
+                          .backDescription)),
+              Expanded(flex: 2, child: _showDificultyOptions())
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   Visibility _showFront() {
     return Visibility(
-                visible: !controller.showAnswer,
-                child: Expanded(
-                  child: Card(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(flex: 16, child: _showImageInDialog(controller.frontCardFile, controller.cardsToStudy[controller.indexCardShowing],controller.cardsToStudy[controller.indexCardShowing].frontDescription)),
-                        Expanded(flex: 1,
-                          child: ElevatedButton(
-                            onPressed: () => setState(() {
-                              controller.showAnswer = !controller.showAnswer;
-                            }), 
-                            child: const Icon(Icons.arrow_downward)
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
+      visible: !controller.showAnswer,
+      child: Expanded(
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 16,
+                  child: _showImageInDialog(
+                      controller.frontCardFile,
+                      controller.cardsToStudy[controller.indexCardShowing],
+                      controller.cardsToStudy[controller.indexCardShowing]
+                          .frontDescription)),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: () => setState(() {
+                          controller.showAnswer = !controller.showAnswer;
+                        }),
+                    child: const Icon(Icons.arrow_downward)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   Row _showDificultyOptions() {
@@ -142,36 +151,37 @@ class _StudyCardsPageState extends State<StudyCardsPage>{
     );
   }
 
-  Expanded _dificulltyButton(Duration duration, String name, String durationString) {
-    return Expanded(child: ElevatedButton(
-        onPressed: () => setState(() {
-          controller.cardsToStudy[controller.indexCardShowing].timeToStudy = DateTime.now().add(duration);
-          FileManager.instance.saveCards();
-          controller.cardsStudied.add(controller.indexCardShowing);
-          controller.nextCard();
-        }), 
-        child: ListTile(
-          title: Text(name,textAlign: TextAlign.center, maxLines: 1,),
-          subtitle: Text(durationString),
-        )
-        ));
+  Expanded _dificulltyButton(
+      Duration duration, String name, String durationString) {
+    return Expanded(
+        child: ElevatedButton(
+            onPressed: () => setState(() {
+                  controller.cardsToStudy[controller.indexCardShowing]
+                      .timeToStudy = DateTime.now().add(duration);
+                  FileManager.instance.saveCards();
+                  controller.cardsStudied.add(controller.indexCardShowing);
+                  controller.nextCard();
+                }),
+            child: ListTile(
+              title: Text(
+                name,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+              ),
+              subtitle: Text(durationString),
+            )));
   }
 
-  Widget _showImageInDialog(File? file, CardModel card, String text){
-    
-    if(file != null) {
+  Widget _showImageInDialog(File? file, CardModel card, String text) {
+    if (file != null) {
       return Column(
         children: [
-          Expanded(flex:1,child: Text(text)),
-          Expanded(flex:8,child: Image.file(file)),
+          Expanded(flex: 1, child: Text(text)),
+          Expanded(flex: 8, child: Image.file(file)),
         ],
       );
     } else {
       return Text(text);
     }
-
   }
-
-
-
 }
