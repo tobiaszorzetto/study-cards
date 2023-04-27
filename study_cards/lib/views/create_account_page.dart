d618909ac7752fa3ce55118d9a34dcaae785e766
tobiaszorzetto <66502140+tobiaszorzetto@users.noreply.github.com>
2023-04-27 10:45:17 -0300

    docs criado

--- study_cards/lib/views/create_account_page.dart
+++ study_cards/lib/views/create_account_page.dart
@@ -15,34 +15,96 @@ class _CreateAccountState extends State<CreateAccount>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(labelText: "email"),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [Colors.blue, Colors.black])),
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width / 4,
+            height: MediaQuery.of(context).size.height / 2,
+            child: Card(
+              elevation: 0,
+              color: Colors.transparent,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(child: Image.asset("assets/images/logo.png")),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        hintText: "email",
+                        enabledBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                        icon: Icon(Icons.email),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "password",
+                          border: OutlineInputBorder(),
+                          icon: Icon(Icons.password),
+                          filled: true,
+                          fillColor: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: confirmPasswordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "confirm password",
+                          border: OutlineInputBorder(),
+                          icon: Icon(Icons.password),
+                          filled: true,
+                          fillColor: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                                  backgroundColor: const Color(0xFF2697FF),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 14.0, horizontal: 80),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(12.0))),
+                        onPressed: () => setState(() {
+                              createUser();
+                            }),
+                        child: const Text(
+                                "Create Account",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  letterSpacing: 0.5,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                        ),
+                    ),
+                    
+                  ]),
             ),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(labelText: "password"),
-            ), 
-            TextField(
-              controller: confirmPasswordController,
-              decoration: const InputDecoration(labelText: "confirm password"),
-            ), 
-            ElevatedButton(
-              onPressed: () => setState(() {
-                createUser();
-              }), 
-              child: Text("Create account")
-            ),
-          ]
+          ),
         ),
       ),
     );
   }
 
   Future createUser() async {
+    if(passwordController.text != confirmPasswordController.text) return;
     try {
       final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: emailController.text,
